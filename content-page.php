25dea2545d21282356b8fde4ba1d815d01e2531c
cbdweb <nik@cbdweb.net>
2019-01-04 22:13:05 +1100

    reintroduce page heading not on front page;

--- content-page.php
+++ content-page.php
@@ -15,6 +15,11 @@
 	<div class="entry-content <?=is_page_template( 'full-width.php' ) ? ' full-width' : ''?> ">
             <?php if ( has_post_thumbnail() ) { ?>
                 <img class="featured-image" src="<?=wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]?>"/>
+            <?php }
+            if( ! is_front_page() ){ ?>
+                <header class="entry-header">
+                    <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+                </header><!-- .entry-header -->
             <?php } ?>
             <?php the_content(); ?>
 	</div><!-- .entry-content -->
