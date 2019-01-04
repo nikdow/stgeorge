cb01aa45107a17b6d0703addcc4c87b2662a2332
cbdweb <nik@cbdweb.net>
2019-01-04 23:30:31 +1100

    move page title;

--- content-page.php
+++ content-page.php
@@ -11,8 +11,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
     <?php include_once "sidebar.php"?>
-    
-    if( ! is_front_page() ){ ?>
+
+    <?php if( ! is_front_page() ){ ?>
     <header class="entry-header">
         <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
     </header><!-- .entry-header -->
@@ -20,7 +20,7 @@
 	<div class="entry-content <?=is_page_template( 'full-width.php' ) ? ' full-width' : ''?> ">
             <?php if ( has_post_thumbnail() ) { ?>
                 <img class="featured-image" src="<?=wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]?>"/>
-            <?php }
+            <?php } ?>
 
             <?php the_content(); ?>
 	</div><!-- .entry-content -->
